f87028eb99941785eba51c7eb04f8f5633e77d40
Craig Paterson <craig@widdershins.co.za>
2016-09-05 19:21:53 +0200

    fix sample config

--- mix.exs
+++ mix.exs
@@ -4,6 +4,8 @@ defmodule Infobip.Mixfile do
   def project do
     [app: :infobip,
      version: "0.0.10",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
      deps: deps,
      test_coverage: [tool: ExCoveralls],
      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test],
